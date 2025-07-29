2f4fbc5d2e42db26eb2b7aa286730dea5932a3fb
Vu.Minh.Quan <Quan.VM225910@sis.hust.edu.vn>
2025-07-29 22:44:44 +0700

    Chapter 14

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,3 +38,13 @@ users = User.order(:created_at).take(6)
 end
 
 puts "✅ Created #{Micropost.count} microposts."
+
+users = User.all
+user = users.first
+following = users[2..20]
+followers = users[3..15]
+
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
+
+puts "✅ Created follow relationships."
